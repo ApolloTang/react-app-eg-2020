178f7cc2bb195ebc50749324c396eff603fbd2ce
Apollo Tang <tang.apollo@gmail.com>
2020-03-01 02:03:49 -0500

    yarn install; get it to run

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import img from './react.png';
 import s from './style.module.less';
-import MyButton from '@myscope/ui--button';
-import MyButtonBlue from '@myscope/ui--button-blue';
-import message from '@myscope/util--get-message'
 
 
 
@@ -11,8 +8,6 @@ import FontTest from 'font-test/';
 const App = () => (
   <div className={`${s.app}`}>
     <h1>Hello React</h1>
-    <div><MyButton>{message()}</MyButton> </div>
-    <div><MyButtonBlue>hello Blue Button</MyButtonBlue> </div>
     <div className={`${s.imageContainer}`}><img src={img} /></div>
     <FontTest />
   </div>
