08ea6fe9a6a64f3da9f7f726e9765beeee987ec5
Apollo Tang <tang.apollo@gmail.com>
2020-03-01 23:15:58 -0500

    covert to async await

--- src/pages/users/containers/users-catelogue/actions.js
+++ src/pages/users/containers/users-catelogue/actions.js
@@ -1,40 +1,36 @@
 import { API } from 'app/resources/users-catelogue/services'
 import { actionNames } from 'root/action-names'
 
-console.log('yyy actionNames: ' , actionNames)
-
 const userCatelog = {
+
   init() {
     return (dispatch, getState) => {
-      dispatch({
-        type: actionNames.usersPage_userCatelogue_init
-      })
+      dispatch({ type: actionNames.usersPage_userCatelogue_init })
     }
   },
-  fetchUserCatelog () {
-    return (dispatch, getState) => {
+
+  fetchUserCatelog() {
+    return async (dispatch, getState) =>  {
+
       dispatch({
         type: actionNames.usersPage_userCatelogue_fetch_begin
       })
 
-      return API.userCatelog.getAll().then(
-        userCatelog => {
-          const ids_userCatelog = userCatelog.map( user=>user._id)
-          dispatch({
-            type: actionNames.usersPage_userCatelogue_fetch_success,
-            payload: { ids_userCatelog }
-          })
-        },
-        err=>{
-          dispatch({
-            type: actionNames.usersPage_userCatelogue_fetch_fail,
-            error: err
-          })
-        },
-
-      )
+      try {
+        const userCatelog = await API.userCatelog.getAll()
+        const ids_userCatelog = userCatelog.map( user=>user._id)
+        dispatch({
+          type: actionNames.usersPage_userCatelogue_fetch_success,
+          payload: { ids_userCatelog }
+        })
+      } catch(err) {
+        dispatch({
+          type: actionNames.usersPage_userCatelogue_fetch_fail,
+          error: err
+        })
+      }
     }
-  },
+  }
 }
 
 export default userCatelog
